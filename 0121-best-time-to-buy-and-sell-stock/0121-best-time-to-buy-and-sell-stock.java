a8f29dbde3551231fa94704a62103e278a17f44e
wying2 <42809489+wying2@users.noreply.github.com>
2024-02-02 23:26:57 -0800

    Time: 2 ms (78.65%), Space: 61.9 MB (37.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,7 +4,6 @@ class Solution {
         int max_prof = 0;
         for (int i = 0; i < prices.length; i ++) {
             if (prices[i] < min) min = prices[i];
-            System.out.println(min);
             if (prices[i] - min > max_prof) max_prof = prices[i] - min;
         }
         return max_prof;
