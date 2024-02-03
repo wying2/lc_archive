eb511c10134dc17bdb2b4947c8f169b10f750926
wying2 <42809489+wying2@users.noreply.github.com>
2024-02-02 21:42:00 -0800

    Time: 0 ms (100.00%), Space: 41.1 MB (25.76%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,18 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
-        if (nums.length == 3) return Math.max(Math.max(nums[0], nums[1]), nums[2]);
-        int[] dp1 = new int[nums.length - 1];
-        int[] dp2 = new int[nums.length - 1];
-        dp1[0] = nums[0];
-        dp1[1] = Math.max(nums[0], nums[1]);
-        dp2[0] = nums[1];
-        dp2[1] = Math.max(nums[1], nums[2]);
-        for (int i = 2; i < nums.length - 1; i ++) {
-            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
-            dp2[i] = Math.max(dp2[i - 2] + nums[i + 1], dp2[i - 1]);
+        // if (nums.length == 1) return nums[0];
+        // if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        // if (nums.length == 3) return Math.max(Math.max(nums[0], nums[1]), nums[2]);
+        if (nums.length <= 3) {
+            Arrays.sort(nums);
+            return nums[nums.length - 1];
         }
-        return Math.max(dp1[nums.length - 2], dp2[nums.length - 2]);
+        // int[] dp1 = new int[nums.length - 1];
+        // int[] dp2 = new int[nums.length - 1];
+        int dp10 = nums[0];
+        int dp11 = Math.max(nums[0], nums[1]);
+        int dp20 = nums[1];
+        int dp21 = Math.max(nums[1], nums[2]);
+        for (int i = 2; i < nums.length - 1; i ++) {
+            int dp1i = Math.max(dp10 + nums[i], dp11);
+            int dp2i = Math.max(dp20 + nums[i + 1], dp21);
+            dp10 = dp11;
+            dp11 = dp1i;
+            dp20 = dp21;
+            dp21 = dp2i;
+        }
+        return Math.max(dp11, dp21);
     }
 }
