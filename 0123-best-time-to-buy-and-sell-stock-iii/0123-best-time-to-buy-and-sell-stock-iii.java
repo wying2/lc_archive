ec530e0f3a685e0adb0f826ea3d4f32c61331190
wying2 <42809489+wying2@users.noreply.github.com>
2024-02-03 21:02:17 -0800

    Time: 27 ms (41.01%), Space: 60.1 MB (73.79%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -12,9 +12,6 @@ class Solution {
             dp[i][2] = Math.max(dp[i-1][1]-prices[i], dp[i-1][2]);
             dp[i][3] = Math.max(dp[i-1][2]+prices[i], dp[i-1][3]);
         }
-        for(int[] it : dp) {
-            System.out.println(Arrays.toString(it));
-        }
         return Math.max(dp[prices.length-1][1], dp[prices.length-1][3]);
     }
 }
