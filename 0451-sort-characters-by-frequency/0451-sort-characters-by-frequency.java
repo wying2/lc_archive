37e571fd8dd85c6c423a9e2f1516a19496364694
wying2 <42809489+wying2@users.noreply.github.com>
2024-02-06 22:47:29 -0800

    Time: 17 ms (50.73%), Space: 45.5 MB (56.46%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,23 +1,28 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) 
-            map.put(c, map.getOrDefault(c, 0) + 1);
-						
-        List<Character> [] bucket = new List[s.length() + 1];
-        for (char key : map.keySet()) {
-            int frequency = map.get(key);
-            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
-            bucket[frequency].add(key);
+        Map<Character, Integer> hm = new HashMap();
+        for (int i = 0; i < s.length(); i ++) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
-				
-        StringBuilder sb = new StringBuilder();
-        for (int pos = bucket.length - 1; pos >= 0; pos--)
-            if (bucket[pos] != null)
-                for (char c : bucket[pos])
-                    for (int i = 0; i < pos; i++)
-                        sb.append(c);
+        int maxFreq = Collections.max(hm.values());
+        List<Character> [] buckets = new List[maxFreq + 1];
 
+        for (Character c : hm.keySet()) {
+            int freq = hm.get(c);
+            if (buckets[freq] == null) 
+                buckets[freq] = new ArrayList();
+            buckets[freq].add(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int j = buckets.length - 1; j > 0; j --) {
+            if (buckets[j] != null) {
+                for (Character c : buckets[j]) {
+                    for (int i = 0; i < j; i ++) {
+                        sb.append(c);
+                    }
+                }
+            }
+        }
         return sb.toString();
     }
 }
