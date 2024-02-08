d17aee8b2b2b780ff82422c812b39c8201ae81b0
wying2 <42809489+wying2@users.noreply.github.com>
2024-02-07 22:30:05 -0800

    Time: 12 ms (85.95%), Space: 41.7 MB (92.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,42 +1,22 @@
 class Solution {
-    
     public int numDistinct(String s, String t) {
-
-        int M = s.length();
-        int N = t.length();
-        
-        int[] dp = new int[N];
-        
+        int[] dp = new int[t.length()];
         int prev = 1;
-        
-        // Iterate over the strings in reverse so as to
-        // satisfy the way we've modeled our recursive solution
-        for (int i = M - 1; i >= 0; i--) {
-                
-                // At each step we start with the last value in
-                // the row which is always 1. Notice how we are
-                // starting the loop from N - 1 instead of N like
-                // in the previous solution.
-                prev = 1;
-
-                for (int j = N - 1; j >= 0; j--) {
-
-                    // Record the current value in this cell so that
-                    // we can use it to calculate the value of dp[j - 1]
-                    int old_dpj = dp[j];
-
-                    // If the characters match, we add the
-                    // result of the next recursion call (in this
-                    // case, the value of a cell in the dp table
-                    if (s.charAt(i) == t.charAt(j)) {
-                        dp[j] += prev;
-                    }
-
-                    // Update the prev variable
-                    prev = old_dpj;    
+        for (int i = s.length() - 1; i >= 0; i --) {
+            prev = 1;
+            for (int j = t.length() - 1; j >= 0; j --) {
+                int old_dpj = dp[j];
+                if (s.charAt(i) == t.charAt(j))
+                    dp[j] += prev;
+                prev = old_dpj;
             }
         }
-        
         return dp[0];
     }
 }
+
+// dp[i-1][j-1] => # of subsequences of s[1:i], t[1:j]
+// if s[i] == t[j]
+// dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+// else
+// dp[i][j] = dp[i-1][j]
