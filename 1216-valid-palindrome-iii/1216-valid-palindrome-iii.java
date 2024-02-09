64d98e28c9fea3d1affc2148dfad20f73335f9d0
wying2 <42809489+wying2@users.noreply.github.com>
2024-02-08 21:26:43 -0800

    Time: 17 ms (35.69%), Space: 45.6 MB (41.23%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.java
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.java
@@ -2,16 +2,16 @@ class Solution {
     public boolean isValidPalindrome(String s, int k) {
         int len = s.length();
         int[][] dp = new int[len+1][len+1];
-        StringBuilder sb = new StringBuilder();
-        sb.append(s);
-        String reversed_s = sb.reverse().toString();
+        // StringBuilder sb = new StringBuilder();
+        // sb.append(s);
+        // String reversed_s = sb.reverse().toString();
         for (int i = 0; i <= len; i ++ ) {
             for (int j = 0; j <= len; j ++) {
                 if (i == 0) 
                     dp[i][j] = j;
                 else if (j == 0)
                     dp[i][j] = i;
-                else if (s.charAt(i-1) == reversed_s.charAt(j-1))
+                else if (s.charAt(i-1) == s.charAt(len-j))
                     dp[i][j] = dp[i-1][j-1];
                 else 
                     dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + 1;
