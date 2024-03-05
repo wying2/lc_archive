119668e8d782fe1caeff148f825ee62d6596c320
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-04 20:53:23 -0800

    Time: 20 ms (11.69%), Space: 11.7 MB (66.23%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reachingPoints(self, sx, sy, tx, ty):
+        """
+        :type sx: int
+        :type sy: int
+        :type tx: int
+        :type ty: int
+        :rtype: bool
+        """
+        while tx >= sx and ty >= sy:
+            if tx == ty:
+                break
+            elif tx > ty:
+                if ty > sy:
+                    tx %= ty
+                else:
+                    return (tx - sx) % ty == 0
+            else:                
+                if tx > sx:
+                    ty %= tx
+                else:
+                    return (ty - sy) % tx == 0
+        return tx == sx and ty == sy
