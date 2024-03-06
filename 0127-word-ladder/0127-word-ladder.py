05c4cdb9707cc4cede621d466a0d4f90ce1dd044
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-05 17:59:42 -0800

    Time: 347 ms (47.71%), Space: 18.3 MB (68.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,15 +1,41 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
         wordList = set(wordList)
-        queue = collections.deque([[beginWord, 1]])
+        if endWord not in wordList:
+            return 0
+        
+        def diffByOne (word1, word2):
+            count = 0
+            for i in range(len(word1)):
+                if word1[i] != word2[i]:
+                    count += 1
+            return count <= 1
+        
+        graph = {}
+        for word in wordList:
+            if word not in graph:
+                graph[word] = []
+        
+        queue = []
+        visited = set()
+        queue.append(beginWord)
+        visited.add(beginWord)
+        n = len(beginWord)
+        steps = 1
+        alph = "abcdefghijklmnopqrstuvwxyz"
         while queue:
-            word, length = queue.popleft()
-            if word == endWord:
-                return length
-            for i in range(len(word)):
-                for c in 'abcdefghijklmnopqrstuvwxyz':
-                    next_word = word[:i] + c + word[i+1:]
-                    if next_word in wordList:
-                        wordList.remove(next_word)
-                        queue.append([next_word, length + 1])
+            nex = []
+            for word in queue:
+                if word == endWord:
+                    return steps
+                for i in range(len(word)):
+                    for c in alph:
+                        new_word = word[:i] + c + word[i+1:]
+                        if new_word in wordList and new_word not in visited:
+                            wordList.remove(new_word)
+                            visited.add(new_word)
+                            nex.append(new_word)
+            queue = nex
+            steps += 1
         return 0
