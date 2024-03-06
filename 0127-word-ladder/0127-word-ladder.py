8b2615a08ed55f23ea7b1392a8d0a4b16287e9fa
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-05 18:00:27 -0800

    Time: 331 ms (50.80%), Space: 17.9 MB (82.60%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -4,23 +4,8 @@ class Solution:
         wordList = set(wordList)
         if endWord not in wordList:
             return 0
-        
-        def diffByOne (word1, word2):
-            count = 0
-            for i in range(len(word1)):
-                if word1[i] != word2[i]:
-                    count += 1
-            return count <= 1
-        
-        graph = {}
-        for word in wordList:
-            if word not in graph:
-                graph[word] = []
-        
         queue = []
-        visited = set()
         queue.append(beginWord)
-        visited.add(beginWord)
         n = len(beginWord)
         steps = 1
         alph = "abcdefghijklmnopqrstuvwxyz"
@@ -32,9 +17,8 @@ class Solution:
                 for i in range(len(word)):
                     for c in alph:
                         new_word = word[:i] + c + word[i+1:]
-                        if new_word in wordList and new_word not in visited:
+                        if new_word in wordList:
                             wordList.remove(new_word)
-                            visited.add(new_word)
                             nex.append(new_word)
             queue = nex
             steps += 1
