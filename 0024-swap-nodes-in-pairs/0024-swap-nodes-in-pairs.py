543ba657f5a3ba2c02eca81b5df77e4f85e52672
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-10 22:03:59 -0700

    Time: 12 ms (77.02%), Space: 11.7 MB (70.82%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        dummy_head = ListNode(-1, head)
+        pre = dummy_head
+        left = head
+        right = head.next
+        while left and right:
+            pre.next = right
+            left.next = right.next
+            right.next = left
+            pre = left
+            left = pre.next
+            if left:
+                right = left.next
+        
+        return dummy_head.next
+            
