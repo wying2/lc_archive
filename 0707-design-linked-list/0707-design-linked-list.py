ea456a3a0ba251f2e45cdfec13b09ba2d0f5d7f2
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-10 21:21:39 -0700

    Time: 195 ms (52.71%), Space: 12.5 MB (65.88%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -1,80 +1,49 @@
-debug = False
-
 class MyLinkedList(object):
     class ListNode:
         def __init__(self, val=0, next=None):
             self.val = val
             self.next = next
+            
 
     def __init__(self):
+        self.dummy_head = ListNode()
         self.size = 0
-        self.head = None
+        
 
-    # def print_list(self):
-    #     curr = self.head
-    #     list_arr = []
-    #     while curr:
-    #         list_arr.append(curr.val)
-    #         curr = curr.next
-    #     print(list_arr)
-    
     def get(self, index):
         """
         :type index: int
         :rtype: int
         """
-        # print(index, self.size)
-        # print_list()
-        if index < self.size:
-            curr = self.head
-            for i in range(index):
-                curr = curr.next
-            return curr.val
-        if debug:
-            curr = self.head
-            list_arr = []
-            while curr:
-                list_arr.append(curr.val)
-                curr = curr.next
-            print(list_arr)
-        return -1
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.dummy_head.next
+        
+        for _ in range(index):
+            curr = curr.next
+        
+        return curr.val
 
     def addAtHead(self, val):
         """
         :type val: int
         :rtype: None
         """
-        new_head = ListNode(val, self.head)
-        self.head = new_head
+        self.dummy_head.next = ListNode(val, self.dummy_head.next)
         self.size += 1
-        if debug:
-            curr = self.head
-            list_arr = []
-            while curr:
-                list_arr.append(curr.val)
-                curr = curr.next
-            print(list_arr)
+        
 
     def addAtTail(self, val):
         """
         :type val: int
         :rtype: None
         """
+        curr = self.dummy_head
+        while curr.next:
+            curr = curr.next
+        curr.next = ListNode(val, None)
         self.size += 1
-        if not self.head:
-            self.head = ListNode(val, None)
-        else:
-            curr = self.head
-            while curr and curr.next:
-                curr = curr.next
-            curr.next = ListNode(val, None)
-        if debug:
-            curr = self.head
-            list_arr = []
-            while curr:
-                list_arr.append(curr.val)
-                curr = curr.next
-            print(list_arr)
+        
 
     def addAtIndex(self, index, val):
         """
@@ -82,47 +51,27 @@ class MyLinkedList(object):
         :type val: int
         :rtype: None
         """
-        if index == 0:
-            self.addAtHead(val)
-        else:
-            if index <= self.size:
-                curr = self.head
-                for i in range(index-1):
-                    curr = curr.next
-                tmp = ListNode(val, curr.next)
-                curr.next = tmp
-                self.size += 1
-        if debug:
-            curr = self.head
-            list_arr = []
-            while curr:
-                list_arr.append(curr.val)
-                curr = curr.next
-            print(list_arr)
+        if index < 0 or index > self.size:
+            return
+        curr = self.dummy_head
+        for _ in range(index):
+            curr = curr.next
+        curr.next = ListNode(val, curr.next)
+        self.size += 1
+        
 
     def deleteAtIndex(self, index):
         """
         :type index: int
         :rtype: None
         """
-        if index < self.size:
-            if index == 0:
-                self.head = self.head.next
-                self.size -= 1
-            else:
-                curr = self.head
-                for i in range(index-1):
-                    curr = curr.next
-                if curr.next:
-                    curr.next = curr.next.next
-                    self.size -= 1
-        if debug:
-            curr = self.head
-            list_arr = []
-            while curr:
-                list_arr.append(curr.val)
-                curr = curr.next
-            print(list_arr)
+        if index < 0 or index >= self.size:
+            return
+        curr = self.dummy_head
+        for _ in range(index):
+            curr = curr.next
+        curr.next = curr.next.next
+        self.size -= 1
 
 
 # Your MyLinkedList object will be instantiated and called as such:
