fe7008b149512cb60b80b52c127cca71a7f64201
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-10 21:48:29 -0700

    Time: 23 ms (22.25%), Space: 18.1 MB (6.47%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -18,10 +18,19 @@ class Solution(object):
             cur = nex
         return pre
             
+    def recursive(self, head):
+        def helper(pre, cur):
+            if not cur:
+                return pre
+            nex = cur.next
+            cur.next = pre
+            return helper(cur, nex)
+        return helper(None, head)
     
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        return self.twoPointers(head)
+        # return self.twoPointers(head)
+        return self.recursive(head)
