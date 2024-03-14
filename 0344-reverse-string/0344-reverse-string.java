91732d9e52f99edaa3a38eba44ba833525e3e398
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-14 10:13:12 -0700

    Time: 0 ms (100.00%), Space: 49.2 MB (31.33%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -3,9 +3,9 @@ class Solution {
         int left = 0;
         int right = s.length-1;
         while (left < right) {
-            char tmp = s[left];
-            s[left] = s[right];
-            s[right] = tmp;
+            s[left] ^= s[right];
+            s[right] ^= s[left];
+            s[left] ^= s[right];
             left ++;
             right --;
         }
