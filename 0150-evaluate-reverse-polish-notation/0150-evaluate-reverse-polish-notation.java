7b65f7f016f1a9639001fc77093ae66e1d6b8ce4
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-22 19:02:13 -0700

    Time: 5 ms (93.08%), Space: 44.3 MB (88.66%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,22 +1,20 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         String ops = "+-*/";
-        Deque<String> stack = new LinkedList<>();
+        Deque<Integer> stack = new LinkedList<>();
         for (String t : tokens) {
-            if (!ops.contains(t)) stack.push(t);
+            if (!ops.contains(t)) stack.push(Integer.valueOf(t));
             else {
                 // System.out.println(stack);
                 // System.out.println(t.equals("+"));
-                String num2_str = stack.pop();
-                String num1_str = stack.pop();
-                int num1 = Integer.valueOf(num1_str);
-                int num2 = Integer.valueOf(num2_str);
-                if (t.equals("+")) stack.push(Integer.toString(num1+num2));
-                else if (t.equals("-")) stack.push(Integer.toString(num1-num2));
-                else if (t.equals("*")) stack.push(Integer.toString(num1*num2));
-                else if (t.equals("/")) stack.push(Integer.toString(num1/num2));
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                if (t.equals("+")) stack.push(num1+num2);
+                else if (t.equals("-")) stack.push(num1-num2);
+                else if (t.equals("*")) stack.push(num1*num2);
+                else if (t.equals("/")) stack.push(num1/num2);
             }
         }
-        return Integer.valueOf(stack.pop());
+        return stack.pop();
     }
 }
