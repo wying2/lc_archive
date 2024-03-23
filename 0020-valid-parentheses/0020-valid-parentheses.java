83f33cfdd434f9bde04bd014662b769c386094c4
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-22 18:25:35 -0700

    Time: 1 ms (98.68%), Space: 41.3 MB (77.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
-            if (c == '(') stack.addLast(')');
-            else if (c == '[') stack.addLast(']');
-            else if (c == '{') stack.addLast('}');
-            else if (stack.isEmpty() || stack.getLast() != c) return false;
-            else {stack.removeLast();}
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.peek() != c) return false;
+            else {stack.pop();}
         }
         return stack.isEmpty();
     }
