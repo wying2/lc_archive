349cb0d5248bd4574074ead0a519818bcadd9261
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-23 16:23:03 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (76.29%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,18 +14,20 @@
  * }
  */
 class Solution {
-    // public traverse (TreeNode root, List<Integer> res) {
-    //     if (root == null) return res;
-    //     res.add(root.val);
-    //     res.addAll
-    // }
+    public void traverse (TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        traverse(root.left, res);
+        traverse(root.right, res);
+    }
     
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new LinkedList<>();
-        if (root == null) return res;
-        res.add(root.val);
-        res.addAll(preorderTraversal(root.left));
-        res.addAll(preorderTraversal(root.right));
+        // if (root == null) return res;
+        // res.add(root.val);
+        // res.addAll(preorderTraversal(root.left));
+        // res.addAll(preorderTraversal(root.right));
+        traverse(root, res);
         return res;
     }
 }
