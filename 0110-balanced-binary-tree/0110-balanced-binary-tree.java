76e544a6d3ecbd3e3d374a7f3a9a07b89c0fedd8
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-29 22:44:51 -0700

    Time: 0 ms (100.00%), Space: 44.7 MB (14.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,19 +16,16 @@
 class Solution {
     int findHeight (TreeNode root) {
         if (root == null) return 0;
-        return Math.max(findHeight(root.left), findHeight(root.right)) + 1;
+        int leftHeight = findHeight(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = findHeight(root.right);
+        if (rightHeight == -1) return -1;
+        int res = Math.abs(leftHeight - rightHeight) > 1 ? -1 : Math.max(leftHeight, rightHeight) + 1;
+        return res;
     }
     
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        else if (root.left == null) return findHeight(root.right) <= 1;
-        else if (root.right == null) return findHeight(root.left) <= 1;
-        else {
-            boolean leftBalanced = isBalanced(root.left);
-            boolean rightBalanced = isBalanced(root.right);
-            int leftHeight = findHeight(root.left);
-            int rightHeight = findHeight(root.right);
-            return Math.abs(leftHeight-rightHeight) <= 1 && leftBalanced && rightBalanced;
-        }
+        boolean res = findHeight(root) > -1 ? true : false;
+        return res;
     }
 }
