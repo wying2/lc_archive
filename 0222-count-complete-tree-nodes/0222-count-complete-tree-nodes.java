d43ebe4dbe6589fbcf7fe6db5f88da62917fe4aa
wying2 <42809489+wying2@users.noreply.github.com>
2024-03-29 20:29:30 -0700

    Time: 4 ms (7.11%), Space: 46.7 MB (97.37%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,9 +15,18 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if (root == null) return 0;
-        int leftNum = countNodes(root.left);
-        int rightNum = countNodes(root.right);
-        return leftNum + rightNum + 1;
+        Queue<TreeNode> q = new LinkedList();
+        if (root != null) q.offer(root);
+        int count = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i ++) {
+                TreeNode curr = q.poll();
+                count += 1;
+                if (curr.left != null) q.offer(curr.left);
+                if (curr.right != null) q.offer(curr.right);
+            }
+        }
+        return count;
     }
 }
