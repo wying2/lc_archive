4c986b288395a179258f5aab9d0a67300062cba4
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-02 15:08:42 -0700

    Time: 447 ms (91.48%), Space: 20.3 MB (10.30%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,20 +1,42 @@
+class UnionFind:
+    def __init__ (self, n:int):
+        self.father = [i for i in range(n)]
+    
+    def find (self, u):
+        if u != self.father[u]:
+            self.father[u] = self.find(self.father[u])
+        return self.father[u]
+    
+    def isSame(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        return u == v
+    
+    def join(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        if (u == v):
+            return
+        self.father[v] = u
+        
+
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        m, n = len(heights[0]), len(heights)
-        left, right, ans = 0, 10**6 - 1, 0
-        while left <= right:
-            mid = (left + right) // 2
-            q = [(0,0)]
-            seen = {(0,0)}
-            while q:
-                x, y = q.pop()
-                for nx, ny in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                    if 0 <= nx < n and 0 <= ny < m and (nx, ny) not in seen and abs(heights[nx][ny] - heights[x][y]) <= mid:
-                        q.append((nx, ny))
-                        seen.add((nx, ny))
-            if (n-1, m-1) in seen:
-                ans = mid
-                right = mid - 1
-            else:
-                left = mid + 1
+        m, n = len(heights), len(heights[0])
+        edges = []
+        for i in range(m):
+            for j in range(n):
+                ind = i * n + j
+                if i > 0:
+                    edges.append((ind - n, ind, abs(heights[i][j] - heights[i-1][j])))
+                if j > 0:
+                    edges.append((ind - 1, ind, abs(heights[i][j] - heights[i][j-1])))
+        edges.sort(key=lambda e : e[2])
+        ans = 0
+        uf = UnionFind(m * n)
+        for x, y, v in edges:
+            uf.join(x, y)
+            if uf.isSame(0, m * n - 1):
+                ans = v
+                break
         return ans
