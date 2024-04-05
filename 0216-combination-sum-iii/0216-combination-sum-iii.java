e05c95a19f1584b974d99adee7cd135162648d2d
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-04 18:03:57 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (27.45%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> res;
+    LinkedList<Integer> path;
+    int sum;
+    void backtrack(int k, int n, int startInd) {
+        if (sum == n && path.size() == k) {
+            res.add(new LinkedList(path));
+            return;
+        }
+        for (int i = startInd; i <= 9; i ++) {
+            path.add(i);
+            sum += i;
+            backtrack(k, n, i + 1);
+            path.removeLast();
+            sum -= i;
+        }
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        this.res = new LinkedList();
+        this.path = new LinkedList();
+        this.sum = 0;
+        backtrack(k, n, 1);
+        return res;
+    }
+}
