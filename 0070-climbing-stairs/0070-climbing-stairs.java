6df0939c4d8550625377c96fb07091a4268ec850
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-05 17:52:48 -0700

    Time: 0 ms (100.00%), Space: 40.3 MB (36.80%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
+        int[] dp = new int[3];
         if (n <= 2) return n;
-        for (int i = 0; i <= n; i ++) {
+        for (int i = 1; i <= n; i ++) {
             if (i <= 2) dp[i] = i;
             else {
-                dp[i] = dp[i-1] + dp[i-2];
+                int sum = dp[1] + dp[2];
+                dp[1] = dp[2];
+                dp[2] = sum;
             }
         }
-        return dp[n];
+        return dp[2];
     }
 }
 
