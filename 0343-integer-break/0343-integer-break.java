bf895514aabe8fbb7216419c14642816a0facb07
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-05 20:56:22 -0700

    Time: 1 ms (44.95%), Space: 40.2 MB (66.11%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[n+1];
         dp[1] = 1;
         for (int i = 0; i <= n; i ++) {
-            for (int j = 0; j < i; j ++) {
+            for (int j = 1; j < i; j ++) {
                 dp[i] = Math.max(dp[i], Math.max(j * (i-j), dp[j]*(i-j)));
             }
             // System.out.println(Arrays.toString(dp));
@@ -14,7 +14,7 @@ class Solution {
 
 // dp[i]: max product i can get for i break
 // size: n
-// dp[i] = max (foreach j in range i, dp[j] * (i-j))
+// dp[i] = max (foreach j in range i, dp[j] * (i-j), j * (i-j))
 // dp[0] = 0
 // dp[1] = 1
 // dp[2] = 1
