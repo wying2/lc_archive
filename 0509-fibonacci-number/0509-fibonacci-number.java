5b85621bad9a32bb694944f117fbae0bf9c555f5
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-05 17:42:13 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (26.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,15 @@
 class Solution {
     public int fib(int n) {
         if (n == 0) return 0;
-        int[] dp = new int[n+1];
+        int[] dp = new int[2];
         dp[0] = 0;
         dp[1] = 1;
         for (int i = 2; i <= n; i ++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int sum = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = sum;
         }
-        return dp[n];
+        return dp[1];
     }
 }
 
