425f90a3d162e1d4020810715515799fff6a1aa8
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-06 20:09:14 -0700

    Time: 9 ms (98.89%), Space: 46.6 MB (9.03%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,29 +1,23 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        // sort jobs by difficulty
-        int n = difficulty.length;
-        int m = worker.length;
-        int[][] jobs = new int[n][2];
-        for (int i = 0; i < n; i ++) {
-            jobs[i] = new int[]{difficulty[i], profit[i]};
+        int size = (int)Math.pow(10, 5) + 1;
+        int[] dp = new int[size];
+        for (int i = 0; i < difficulty.length; i ++) {
+            dp[difficulty[i]] = Math.max(dp[difficulty[i]], profit[i]);
         }
-        Arrays.sort(jobs, (a, b) -> (a[0] - b[0]));
-        Arrays.sort(worker);
-        // for (int i = 0; i < jobs.length; i ++) {
-        //     System.out.println(Arrays.toString(jobs[i]));
-        // }
-        System.out.println(Arrays.toString(worker));
+        for (int i = 1; i < size; i ++) {
+            dp[i] = Math.max(dp[i-1], dp[i]);
+        }
         int sum = 0;
-        int j = -1;
-        int max = 0;
-        for (int i = 0; i < m; i ++) {
-            while (j+1 < n && jobs[j+1][0] <= worker[i]) {
-                j ++;
-                max = Math.max(max, jobs[j][1]);
-            }
-            if (j != -1)
-                sum += max;
+        for (int i = 0; i < worker.length; i ++) {
+            sum += dp[worker[i]];
         }
         return sum;
     }
 }
+
+// dp[i]: max profit for 1 job for ability i
+// size: 10 ** 5 + 1
+// for i in difficulty: dp[i] = max(profit[i])
+// dp[i] = max(dp[i], dp[i-1])
+// return sum(dp[j] for j in worker)
