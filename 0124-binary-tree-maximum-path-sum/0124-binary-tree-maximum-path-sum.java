8a2a8b8f2323b09a582c9fffc2894c178177025c
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-06 23:49:02 -0700

    Time: 0 ms (100.00%), Space: 43.9 MB (91.33%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -16,30 +16,15 @@
 class Solution {
     int max;
     int traversal(TreeNode curr) {
-        int curr_max = -1001;
-        int left = -1001;
-        int right = -1001;
-        if (curr.left == null && curr.right == null) {
-            curr_max = curr.val;
-        } else if (curr.right == null) {
-            left = traversal(curr.left);
-            curr_max = Math.max(left+curr.val, curr.val);
-        } else if (curr.left == null) {
-            right = traversal(curr.right);
-            curr_max = Math.max(right+curr.val, curr.val);
-        } else {
-            left = traversal(curr.left);
-            right = traversal(curr.right);
-            curr_max = Math.max(Math.max(left+curr.val, right+curr.val), curr.val);
-        }
-        int[] max_candidates = new int[]{left+right+curr.val, curr_max, left, right};
-        for (int i = 0; i < max_candidates.length; i ++)
-            max = Math.max(max, max_candidates[i]);
+        if (curr == null) return 0;
+        int left = Math.max(traversal(curr.left), 0);
+        int right = Math.max(traversal(curr.right), 0);
+        int curr_max = Math.max(left, right) + curr.val;
+        max = Math.max(left + right + curr.val, max);
         return curr_max;
     }
     public int maxPathSum(TreeNode root) {
         this.max = -1001;
-        if (root == null) return 0;
         traversal(root);
         return max;
     }
