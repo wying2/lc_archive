7f390669d0b978ee278dc5ee05f147e28276b230
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-07 18:25:15 -0700

    Time: 35 ms (22.48%), Space: 81.4 MB (73.06%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -0,0 +1,50 @@
+class Solution {
+    static int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        int[][] visited = new int[m][n];
+        int count = 0;
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (grid2[i][j] == 0 || visited[i][j] == 1)
+                    continue;
+                // System.out.println("i = " + i + " j = " + j);
+                boolean tmp = bfs(visited, grid1, grid2, i, j);
+                // System.out.println("bfs = " + tmp);
+                if (tmp) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+    private boolean bfs(int[][] visited, int[][] grid1, int[][] grid2, int i, int j) {
+        boolean contains_all = true;
+        int m = grid1.length;
+        int n = grid1[0].length;
+        Queue<int[]> q = new LinkedList();
+        contains_all = grid1[i][j] == 1;
+        q.offer(new int[]{i, j});
+        visited[i][j] = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int k = 0; k < size; k ++) {
+                int[] curr = q.poll();
+                for (int[] dir : this.dirs) {
+                    int ni = curr[0] + dir[0];
+                    int nj = curr[1] + dir[1];
+                    if (ni < 0 || ni >= m ||
+                        nj < 0 || nj >= n ||
+                        visited[ni][nj] == 1 ||
+                        grid2[ni][nj] == 0)
+                        continue;
+                    if (grid1[ni][nj] == 0) contains_all = false;
+                    q.offer(new int[]{ni, nj});
+                    visited[ni][nj] = 1;
+                }
+            }
+        }
+        return contains_all;
+    }
+}
