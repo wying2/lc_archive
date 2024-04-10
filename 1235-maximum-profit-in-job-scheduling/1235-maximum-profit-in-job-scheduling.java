3dd70a9e4a9a4782d7cbe466f5d28b99fd60957b
wying2 <42809489+wying2@users.noreply.github.com>
2024-04-10 08:30:46 -0700

    Time: 34 ms (75.16%), Space: 55.8 MB (69.97%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -8,21 +8,25 @@ class Solution {
         Arrays.sort(jobs, (a, b) -> (a[1] - b[1]));
         int[] dp = new int[n+1];
         for (int i = 1; i <= n; i ++) {
-            int k = binarySearch(jobs, i-1, jobs[i-1][0]);
-            dp[i] = Math.max(dp[i-1], dp[k+1] + jobs[i-1][2]);
+            int k = binarySearchLeft(jobs, i-1, jobs[i-1][0]);
+            dp[i] = Math.max(dp[i-1], dp[k + 1] + jobs[i-1][2]);
+            // System.out.println(Arrays.toString(dp));
         }
         return dp[n];
     }
-    private int binarySearch(int[][] jobs, int right, int target) {
+    private int binarySearchLeft(int[][] jobs, int right, int target) {
         int left = 0;
-        while (left < right) {
+        int ind = -1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (jobs[mid][1] <= target)
+            if (jobs[mid][1] <= target) {
                 left = mid + 1;
+                ind = mid;
+            }
             else
-                right = mid;
+                right = mid - 1;
         }
-        return left - 1;
+        return ind;
     }
 }
 
